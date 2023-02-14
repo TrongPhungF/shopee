52fc004d06ab4ad87ca72d6b259f6847dfb2211f
PhungHuynh <huynhtrongphungse@gmail.com>
2023-02-14 16:07:58 +0700

    first commit

---
+++ shopee-account-service/src/main/java/com/org/accountservice/ShopeeAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.org.accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopeeAccountServiceApplication.class, args);
+    }
+
+}
