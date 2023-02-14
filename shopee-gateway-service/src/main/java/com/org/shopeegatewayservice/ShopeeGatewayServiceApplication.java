416b9c57de8c6cc2487d4234199198cd99962ac2
PhungHuynh <huynhtrongphungse@gmail.com>
2023-02-14 16:19:59 +0700

    commit shopee gateway service

---
+++ shopee-gateway-service/src/main/java/com/org/shopeegatewayservice/ShopeeGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.org.shopeegatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeeGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ shopee-gateway-service/src/test/java/com/org/shopeegatewayservice/ShopeeGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.shopeegatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeeGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
