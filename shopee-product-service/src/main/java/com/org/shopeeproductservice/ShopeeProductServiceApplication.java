b11ac9344a2465904786896b8b93c38492428cc1
PhungHuynh <huynhtrongphungse@gmail.com>
2023-02-14 16:15:45 +0700

    commit shopee product-service

---
+++ shopee-product-service/src/main/java/com/org/shopeeproductservice/ShopeeProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.org.shopeeproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeeProductServiceApplication.class, args);
+	}
+
+}
---
+++ shopee-product-service/src/test/java/com/org/shopeeproductservice/ShopeeProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.shopeeproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeeProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
