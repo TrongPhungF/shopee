a17196c0e40456b05ef89c010d870dc88d8c1b22
DESKTOP-OFVQ0S5\Truong <truong.lop9a6@gmail.com>
2023-02-14 16:45:48 +0700

    commit project eureka server, update pom gateway project

---
+++ shopee-uereka-server/src/main/java/com/org/uereka/ShopeeUerekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.org.uereka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeUerekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeeUerekaServerApplication.class, args);
+	}
+
+}
