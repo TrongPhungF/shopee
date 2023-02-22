50f83b5e1647752a679d2bb14066f21bb5aed751
nguyentanloc1998 <69185991+nguyentanloc1998@users.noreply.github.com>
2023-02-22 14:30:17 +0700

    fix

--- shopee-eureka-server-service/src/test/java/com/org/shopeefe/ShopeeUerekaServerApplicationTests.java
+++ shopee-eureka-server-service/src/test/java/com/org/shopeefe/ShopeeUerekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.org.uereka;
+package com.org.shopeefe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- shopee-fe-service/src/test/java/com/org/shopeefe/ShopeeFeApplicationTests.java
+++ shopee-fe-service/src/test/java/com/org/shopeefe/ShopeeFeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.org.uereka;
+package com.org.shopeefe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ShopeeFeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
