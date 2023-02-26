a9898d61d90985b30325a2e84f74bb87dbe0d066
DESKTOP-884BJP1\Truong <truong.lop9a6@gmail.com>
2023-02-26 15:17:13 +0700

    commit get orderDTO

--- shopee-fe-service/src/main/java/com/org/shopeefeservice/controllers/PaymentController.java
+++ shopee-fe-service/src/main/java/com/org/shopeefeservice/controllers/PaymentController.java
@@ -1,21 +1,26 @@
 package com.org.shopeefeservice.controllers;
 
+import com.org.shopeefeservice.dtos.OrderDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class PaymentController {
 
     @PostMapping(value = "/payment")
-    public ModelAndView viewProductDetail(@RequestBody Map<String, String> cart) {
+    public ModelAndView viewProductDetail(@RequestBody ArrayList<OrderDTO> orderDTOtList) {
         System.out.println("Đã vào controller Payment");
-
-        for (Map.Entry<String, String> entry : cart.entrySet()) {
-            System.out.println("Id: " + entry.getKey() + ", Quantity: " + entry.getValue());
+        for (OrderDTO orderDTO : orderDTOtList) {
+            System.out.println(orderDTO.getId());
+            System.out.println(orderDTO.getPrice());
+            System.out.println(orderDTO.getQuantity());
+            System.out.println(orderDTO.getTotal());
         }
 
         ModelAndView model = new ModelAndView("login/login.html");
---
+++ shopee-fe-service/src/main/java/com/org/shopeefeservice/dtos/OrderDTO.java
@@ -0,0 +1,20 @@
+package com.org.shopeefeservice.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDTO {
+
+    private String id;
+    private String price;
+    private String quantity;
+    private String total;
+
+}
